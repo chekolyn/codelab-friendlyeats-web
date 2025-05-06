57c26a3d301784f283fb73faceb143ef639de716
Sergio Aguilar Guerrero <saguilarg@google.com>
2025-05-06 21:36:22 +0000

    Read the list of restaurants from Firestore

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -45,12 +45,37 @@ function applyQueryFilters(q, { category, city, price, sort }) {
   return;
 }
 
-export async function getRestaurants(db = db, filters = {}) {
-  return [];
+function applyQueryFilters(q, { category, city, price, sort }) {
+  if (category) {
+    q = query(q, where("category", "==", category));
+  }
+  if (city) {
+    q = query(q, where("city", "==", city));
+  }
+  if (price) {
+    q = query(q, where("price", "==", price.length));
+  }
+  if (sort === "Rating" || !sort) {
+    q = query(q, orderBy("avgRating", "desc"));
+  } else if (sort === "Review") {
+    q = query(q, orderBy("numRatings", "desc"));
+  }
+  return q;
 }
 
-export function getRestaurantsSnapshot(cb, filters = {}) {
-  return;
+export async function getRestaurants(db = db, filters = {}) {
+  let q = query(collection(db, "restaurants"));
+
+  q = applyQueryFilters(q, filters);
+  const results = await getDocs(q);
+  return results.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+      // Only plain objects can be passed to Client Components from Server Components
+      timestamp: doc.data().timestamp.toDate(),
+    };
+  });
 }
 
 export async function getRestaurantById(db, restaurantId) {
